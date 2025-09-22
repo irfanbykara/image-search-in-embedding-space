8bf857bc16373e2da8b9ba45f6fc88caf0ecdeef
irfanbykara <irfanbykara@gmail.com>
2025-09-22 13:55:04 +0300

    cicd hopefully

--- config/settings.py
+++ config/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "postgres"),   # or the DB you set
-        "USER": os.getenv("POSTGRES_USER", "myuser"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "mypassword123"),
-        "HOST": os.getenv("POSTGRES_HOST", "my-django-db.cvgs0qmwuif4.eu-north-1.rds.amazonaws.com"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        "NAME": os.getenv("POSTGRES_DB" ),   # or the DB you set
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
